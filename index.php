30a04cd4fabfe968b95488cc24fde5a12fc6d739
Gabriel Garus <gabriel.garus@gmail.com>
2014-08-11 14:46:22 +0100

    Update index.php
    conflict

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php 
 // Master Copy of a file
+// size=25px
 include ('getImages.php');
 
-echo "<h1>ok</h1> <br /> <hr />";
+echo "<h1>ok</h1> <br /> <hr /> ";
 
 $images = getImages(null,'cover.jpg');
 
